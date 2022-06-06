fb456a7ac0d4d2051495c44860e8b2448ac51fc4
LucasWMedeiros <66040430+LucasWMedeiros@users.noreply.github.com>
2022-06-06 14:08:47 -0300

    adicionando lista de dados e tela de produtos

---
+++ lib/screen/produtcs_overview_screen.dart
@@ -0,0 +1,30 @@
+import 'package:app_loja/data/DUMMY_DATA.dart';
+import 'package:app_loja/models/produt.dart';
+import 'package:flutter/material.dart';
+
+class ProductsOverviewScreen extends StatelessWidget {
+  final List<Product> loadedProducts = DUMMY_PRODUCTS;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Minha Loja'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: GridView.builder(
+          itemCount: loadedProducts.length,
+          itemBuilder: (ctx, index) => Text(loadedProducts[index].title!),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+          ),
+        ),
+      ),
+    );
+  }
+}
