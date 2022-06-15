e7a518180b214454331c15e6a93143023bc757f5
LucasWMedeiros <66040430+LucasWMedeiros@users.noreply.github.com>
2022-06-15 14:07:00 -0300

    criando modelo order_list

---
+++ lib/models/order_list.dart
@@ -0,0 +1,30 @@
+import 'dart:math';
+
+import 'package:app_loja/models/cart.dart';
+import 'package:app_loja/models/order.dart';
+import 'package:flutter/cupertino.dart';
+
+class OrderList with ChangeNotifier {
+  List<Order> _items = [];
+
+  List<Order> get items {
+    return [..._items];
+  }
+
+  int get itemsCount {
+    return _items.length;
+  }
+
+  void addOrder(Cart cart) {
+    _items.insert(
+      0,
+      Order(
+        id: Random().nextDouble().toString(),
+        total: cart.totalAmount,
+        products: cart.items.values.toList(),
+        date: DateTime.now(),
+      ),
+    );
+    notifyListeners();
+  }
+}
