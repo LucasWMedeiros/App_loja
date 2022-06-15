e33e8e1acc782dd8f7263ff66c1f8d0707fc25a6
LucasWMedeiros <66040430+LucasWMedeiros@users.noreply.github.com>
2022-06-15 14:00:41 -0300

    adicionando informações da tela de detalhe do produto

--- lib/screen/products_detail_screen.dart
+++ lib/screen/products_detail_screen.dart
@@ -2,18 +2,49 @@ import 'package:app_loja/models/produt.dart';
 import 'package:flutter/material.dart';
 
 class ProductDetailScreen extends StatelessWidget {
-
   ProductDetailScreen({
     Key? key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-  final Product product = ModalRoute.of(context)?.settings.arguments as Product;
+    final Product product =
+        ModalRoute.of(context)?.settings.arguments as Product;
     return Scaffold(
       appBar: AppBar(
         title: Text(product.name),
         centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                product.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(height: 10,),
+            Text(
+              'R\$${product.price}',
+              style: const TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(height: 10,),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              width: double.infinity,
+              child: Text(
+                product.description,
+                textAlign: TextAlign.center,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
