1af5148462600205d84d5fd999aa58dfff0b2a9d
LucasWMedeiros <66040430+LucasWMedeiros@users.noreply.github.com>
2022-06-16 09:33:08 -0300

    configurando apresentação de itens nos pedidos realizados

--- lib/components/order_widget.dart
+++ lib/components/order_widget.dart
@@ -2,26 +2,65 @@ import 'package:app_loja/models/order.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-class OrderWidget extends StatelessWidget {
+class OrderWidget extends StatefulWidget {
   final Order order;
 
-  const OrderWidget({
-    Key? key,
-    required this.order
-  }) : super (key: key);
+  const OrderWidget({Key? key, required this.order}) : super(key: key);
 
+  @override
+  State<OrderWidget> createState() => _OrderWidgetState();
+}
+
+class _OrderWidgetState extends State<OrderWidget> {
+  bool _expanded = false;
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: ListTile(
-        title: Text('R\$${order.total.toStringAsFixed(2)}'),
-        subtitle: Text(
-          DateFormat('dd/MM/yyyy hh:mm').format(order.date)
-        ),
-        trailing: IconButton(
-          icon: Icon(Icons.expand_more),
-          onPressed: (){},
-        ),
+      child: Column(
+        children: [
+          ListTile(
+            title: Text('R\$${widget.order.total.toStringAsFixed(2)}'),
+            subtitle:
+                Text(DateFormat('dd/MM/yyyy hh:mm').format(widget.order.date)),
+            trailing: IconButton(
+              icon: const Icon(Icons.expand_more),
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
+            ),
+          ),
+          if (_expanded)
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 15,
+                vertical: 4,
+              ),
+              height: (widget.order.products.length * 25) + 10,
+              child: ListView(
+                children: widget.order.products.map((product) {
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        product.name,
+                        style: const TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        '${product.quantity}X R\$${product.price}',
+                        style: const TextStyle(
+                          fontSize: 18,
+                          color: Colors.grey
+                        ),
+                      )
+                    ],
+                  );
+                }).toList(),
+              ),
+            )
+        ],
       ),
     );
   }
