39debc3edb2cab79e7fa8cd70787d79ddb2ad090
LucasWMedeiros <66040430+LucasWMedeiros@users.noreply.github.com>
2022-08-23 10:52:41 -0300

    configurando metodo DELETE com firebase

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -96,12 +96,20 @@ class ProductList with ChangeNotifier {
     return Future.value();
   }
 
-  void removeProduct(Product product) {
+  Future<void> removeProduct(Product product) async {
     int index = _items.indexWhere((p) => p.id == product.id);
 
     if (index >= 0) {
-      _items.removeWhere((p) => p.id == product.id);
+      final product = _items[index];
+      _items.remove(product);
       notifyListeners();
+
+      final response = await delete(Uri.parse('$_baseUrl/${product.id}.json'));
+
+      if(response.statusCode >= 400) {
+        _items.insert(index, product);
+        notifyListeners();
+      }
     }
   }
 }
