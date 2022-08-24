12f7086089300c1800d336a06a9d0e8e521ca68b
LucasWMedeiros <66040430+LucasWMedeiros@users.noreply.github.com>
2022-08-24 15:09:13 -0300

    adicionando titulo e formatando-o

--- lib/screen/auth_screen.dart
+++ lib/screen/auth_screen.dart
@@ -1,3 +1,7 @@
+// ignore_for_file: deprecated_member_use
+
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class authPage extends StatelessWidget {
@@ -9,20 +13,45 @@ class authPage extends StatelessWidget {
       body: Stack(
         children: [
           Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Color.fromRGBO(215, 117, 255, 0.5),
-                  Color.fromRGBO(255, 188, 117, 0.9)
-                ]
-              )
-            ),
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(colors: [
+              Color.fromRGBO(215, 117, 255, 0.5),
+              Color.fromRGBO(255, 188, 117, 0.9)
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
           ),
           Container(
             width: double.infinity,
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 70
+                  ),
+                  //cascade operator
+                  transform: Matrix4.rotationZ(-8 * pi / 180)..translate(-10.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.deepOrange.shade900,
+                    boxShadow: const [
+                      BoxShadow(
+                        blurRadius: 8,
+                        color: Colors.black26,
+                        offset: Offset(0,2)
+                      )
+                    ]
+                  ),
+                  child: Text(
+                    'Minha Loja',
+                    style: TextStyle(
+                      fontSize: 45,
+                      fontFamily: 'Anton',
+                      color: Theme.of(context).accentTextTheme.headline6?.color
+                    ),
+                  ),
+                )
               ],
             ),
           )
