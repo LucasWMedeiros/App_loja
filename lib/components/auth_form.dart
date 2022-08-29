d5df4bd2a5d2823154c35d6a806808d432ad5d34
LucasWMedeiros <66040430+LucasWMedeiros@users.noreply.github.com>
2022-08-29 15:37:17 -0300

    mudando AnimatedBuilder por AnimatedContainer

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -127,16 +127,14 @@ class _AuthFormState extends State<AuthForm>
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10),
       ),
-      child: AnimatedBuilder(
-        animation: _heightAnimation!,
-        builder: (ctx, childForm) => Container(
+      child:  AnimatedContainer(
+        duration: Duration(milliseconds: 300),
+        curve: Curves.linear,
         padding: const EdgeInsets.all(16),
-        // height: _isLogin() ? 310 : 400,
-        height: _heightAnimation?.value.height ?? (_isLogin() ? 310 : 400),
+        height: _isLogin() ? 310 : 400,
+        // height: _heightAnimation?.value.height ?? (_isLogin() ? 310 : 400),
         width: deviceSize.width * 0.75,
-        child: childForm 
-      ),
-      child: Form(
+        child:  Form(
           key: _formKey,
           child: Column(children: [
             TextFormField(
