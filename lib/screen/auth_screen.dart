3f9c31e141c41b94f6cc9b172d250ee321371ea9
LucasWMedeiros <66040430+LucasWMedeiros@users.noreply.github.com>
2022-08-29 15:57:10 -0300

    adicionando correção de Responsividade

--- lib/screen/auth_screen.dart
+++ lib/screen/auth_screen.dart
@@ -20,44 +20,48 @@ class authScreen extends StatelessWidget {
               Color.fromRGBO(255, 188, 117, 0.9)
             ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
           ),
-          Container(
-            width: double.infinity,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  margin: const EdgeInsets.only(
-                    bottom: 20,
-                  ),
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 10,
-                    horizontal: 70
-                  ),
-                  //cascade operator
-                  transform: Matrix4.rotationZ(-8 * pi / 180)..translate(-10.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.deepOrange.shade900,
-                    boxShadow: const [
-                      BoxShadow(
-                        blurRadius: 8,
-                        color: Colors.black26,
-                        offset: Offset(0,2)
-                      )
-                    ]
-                  ),
-                  child: Text(
-                    'Minha Loja',
-                    style: TextStyle(
-                      fontSize: 45,
-                      fontFamily: 'Anton',
-                      color: Theme.of(context).accentTextTheme.headline6?.color
+          Center(
+            child: SingleChildScrollView(
+              child: Container(
+                width: double.infinity,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(
+                        bottom: 20,
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 70
+                      ),
+                      //cascade operator
+                      transform: Matrix4.rotationZ(-8 * pi / 180)..translate(-10.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.deepOrange.shade900,
+                        boxShadow: const [
+                          BoxShadow(
+                            blurRadius: 8,
+                            color: Colors.black26,
+                            offset: Offset(0,2)
+                          )
+                        ]
+                      ),
+                      child: Text(
+                        'Minha Loja',
+                        style: TextStyle(
+                          fontSize: 45,
+                          fontFamily: 'Anton',
+                          color: Theme.of(context).accentTextTheme.headline6?.color
+                        ),
+                      ),
                     ),
-                  ),
+                    AuthForm(),
+                  ],
                 ),
-                AuthForm(),
-              ],
+              ),
             ),
           )
         ],
