ad58da2f96c18469071496cb8269e904f02091b4
LucasWMedeiros <66040430+LucasWMedeiros@users.noreply.github.com>
2022-08-31 13:50:11 -0300

    adicionando animação no load da imagem e animação Hero

--- lib/components/product_grid_item.dart
+++ lib/components/product_grid_item.dart
@@ -17,10 +17,18 @@ class ProductGridItem extends StatelessWidget {
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         child: GestureDetector(
-          child: Image.network(
-            product.imageUrl,
-            fit: BoxFit.cover,
+          child: Hero(
+            tag: product.id,
+            child: FadeInImage(
+              placeholder: const AssetImage('assets/images/product-placeholder.png'),
+              image: NetworkImage(product.imageUrl),
+              fit: BoxFit.cover,
+            ),
           ),
+          // child: Image.network(
+          //   product.imageUrl,
+          //   fit: BoxFit.cover,
+          // ),
           onTap: () {
             Navigator.of(context)
                 .pushNamed(AppRoutes.PRODUCT_DETAIL, arguments: product);
