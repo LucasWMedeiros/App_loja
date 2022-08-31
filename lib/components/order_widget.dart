e18e377f5863cada14b2f0244da63cdc4ada626d
LucasWMedeiros <66040430+LucasWMedeiros@users.noreply.github.com>
2022-08-31 14:07:37 -0300

    adicionando animação para tela de pedidos

--- lib/components/order_widget.dart
+++ lib/components/order_widget.dart
@@ -15,52 +15,57 @@ class _OrderWidgetState extends State<OrderWidget> {
   bool _expanded = false;
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Column(
-        children: [
-          ListTile(
-            title: Text('R\$${widget.order.total.toStringAsFixed(2)}'),
-            subtitle:
-                Text(DateFormat('dd/MM/yyyy hh:mm').format(widget.order.date)),
-            trailing: IconButton(
-              icon: const Icon(Icons.expand_more),
-              onPressed: () {
-                setState(() {
-                  _expanded = !_expanded;
-                });
-              },
+    final double itemsHeight = (widget.order.products.length * 25) + 10;
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      height: _expanded ? itemsHeight + 80 : 80,
+      child: Card(
+        child: Column(
+          children: [
+            ListTile(
+              title: Text('R\$${widget.order.total.toStringAsFixed(2)}'),
+              subtitle:
+                  Text(DateFormat('dd/MM/yyyy hh:mm').format(widget.order.date)),
+              trailing: IconButton(
+                icon: const Icon(Icons.expand_more),
+                onPressed: () {
+                  setState(() {
+                    _expanded = !_expanded;
+                  });
+                },
+              ),
             ),
-          ),
-          if (_expanded)
-            Container(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 15,
-                vertical: 4,
-              ),
-              height: (widget.order.products.length * 25) + 10,
-              child: ListView(
-                children: widget.order.products.map((product) {
-                  return Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        product.name,
-                        style: const TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        '${product.quantity}X R\$${product.price}',
-                        style: const TextStyle(
-                          fontSize: 18,
-                          color: Colors.grey
+              AnimatedContainer(
+                duration: const Duration(milliseconds: 300),
+                height: _expanded? itemsHeight : 0,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 15,
+                  vertical: 4,
+                ),
+                child: ListView(
+                  children: widget.order.products.map((product) {
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          product.name,
+                          style: const TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
                         ),
-                      )
-                    ],
-                  );
-                }).toList(),
-              ),
-            )
-        ],
+                        Text(
+                          '${product.quantity}X R\$${product.price}',
+                          style: const TextStyle(
+                            fontSize: 18,
+                            color: Colors.grey
+                          ),
+                        )
+                      ],
+                    );
+                  }).toList(),
+                ),
+              )
+          ],
+        ),
       ),
     );
   }
